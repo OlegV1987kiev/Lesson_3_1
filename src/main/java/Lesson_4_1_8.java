c0d6bf54a31a848014ee1add731fbfd3a126b0d3
Oleg <oleg1987kiev@gmail.com>
2019-09-23 14:10:32 +0300

    rename

---
+++ src/main/java/Lesson_4_1_8.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+
+public class Lesson_4_1_8 {
+    public static void main(String args[]) {
+
+        {
+
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 10; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
+    }
+
+
+
